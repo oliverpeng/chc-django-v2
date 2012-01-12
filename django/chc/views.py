c2cda9dfe3c65b415cda655176e3be5405097283
Oliver Peng <oliverpeng@gmail.com>
2012-01-12 13:55:42 -0600

    Add placeholder templates for other pages

--- django/chc/views.py
+++ django/chc/views.py
@@ -5,16 +5,16 @@ def home(request):
 	return render(request, 'home.html')
 
 def join(request):
-	return render(request, 'home.html')
+	return render(request, 'join.html')
 
 def what_we_do(request):
-	return render(request, 'home.html')
+	return render(request, 'what-we-do.html')
 
 def where_we_are(request):
-	return render(request, 'home.html')
+	return render(request, 'where-we-are.html')
 
 def churches(request):
-	return render(request, 'home.html')
+	return render(request, 'churches.html')
 
 def give(request):
-	return render(request, 'home.html')
+	return render(request, 'give.html')
