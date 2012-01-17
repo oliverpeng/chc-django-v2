75f50ba70acd9332310c24cece4906a75de19a44
Oliver Peng <oliverpeng@gmail.com>
2012-01-17 00:33:53 -0600

    Integrate Sign Up page with MailChimp

--- django/chc/urls.py
+++ django/chc/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     
     url(r'^join/$', 'chc.views.join', name='join'),
     url(r'^join/signup/$', 'chc.views.signup', name='signup'),
+    url(r'^join/signup/thankyou/$', 'chc.views.thanks', name='thanks'),
     url(r'^join/pray/$', 'chc.views.pray', name='pray'),
     url(r'^join/sendprayer/$', 'chc.views.sendprayer', name='sendprayer'),
     url(r'^join/chclive/$', 'chc.views.chclive', name='chclive'),
--- django/chc/views.py
+++ django/chc/views.py
@@ -4,19 +4,19 @@ from django.template import RequestContext
 from django import forms
 
 class SignupForm(forms.Form):
-	fname = forms.CharField()
-	lname = forms.CharField()
-	city = forms.CharField()
-	state = forms.CharField()
-	country = forms.CharField()
-	email = forms.EmailField()
-	is_pastor = forms.BooleanField()
-	phone = forms.CharField()
-	church_name = forms.CharField()
-	church_website = forms.CharField()
-	take_offering = forms.BooleanField()
-	contact_me = forms.BooleanField()
-	receive_updates = forms.BooleanField()
+	fname = forms.CharField(required=False, label='First Name')
+	lname = forms.CharField(required=False, label='Last Name')
+	city = forms.CharField(required=False, label="City")
+	state = forms.CharField(required=False, label="State / Province")
+	country = forms.CharField(required=False, label = "Country")
+	email = forms.EmailField(label="Email Address")
+	is_pastor = forms.BooleanField(required=False, label="Check here if you are a pastor.")
+	phone = forms.CharField(required=False, label="Phone")
+	church_name = forms.CharField(required=False, label="Church Name")
+	church_website = forms.URLField(required=False, label="Church Website")
+	take_offering = forms.BooleanField(required=False, label="I'm on board. Our church will take an offering at the next crisis.")
+	contact_me = forms.BooleanField(required=False, label="I am interested. Please contact me.")
+	receive_updates = forms.BooleanField(required=False, label="I would like to receive updates.")
 
 
 def home(request):
@@ -45,17 +45,49 @@ def signup(request):
 			contact_me = form.cleaned_data['contact_me']
 			receive_updates = form.cleaned_data['receive_updates']
 			
-			if is_pastor:
-				from django.core.mail import send_mail
-				message = ""
-    			send_mail("Pastor signed up", message, sender, recipients)
+			#if is_pastor:
+			#	from django.core.mail import send_mail
+			#	message = ""
+			#	send_mail("Pastor signed up", message, sender, recipients)
 
-			return HttpResponseRedirect('/thanks/')
+			import urllib
+			import urllib2
+			import json
+
+			url = 'http://us1.api.mailchimp.com/1.3/'
+			method = 'listSubscribe'    	
+			params = {
+				'apikey': '420524bc6e7e2495ddd87a95dd48a3dd-us1',
+				'id': '9b182b13d8',
+				'email_address': email,
+				'merge_vars': {
+					'FNAME': fname,
+					'LNAME': lname,
+					'CITY': city,
+					'STATE': state,
+					'CHURCH': church_name,
+					'WEBSITE': church_website
+				}
+			}
+			
+			urlencoded_json = urllib.quote( json.dumps(params) );
+			response = urllib2.urlopen("%s?method=%s" %(url, method), urlencoded_json)
+			data = response.read()
+			response.close()
+			result = json.loads(data)
+			raise Exception
+
+			return HttpResponseRedirect( reverse('thanks') )
+		else:
+			pass
 	else:
 		form = SignupForm()
 
 	c = { 'form': form}
 	return render_to_response('join/signup.html', c, context_instance=RequestContext(request))
+
+def thanks(request):
+	return render(request, 'join/thanks.html')
 
 def pray(request):
 	return render(request, 'join/pray.html')
